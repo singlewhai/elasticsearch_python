54a99479937534371e02b676965ec65e5ef0ae6a
singlewhai <n.ratanasuwan@gmail.com>
2016-02-06 15:43:12 -0200

    finish v1 and start v2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, json
 from werkzeug import secure_filename
 from pyelasticsearch import ElasticSearch
 from subprocess import call
-import os
+import os, string
 
 app = Flask(__name__)
 es = ElasticSearch('http://localhost:9200/')
@@ -12,14 +12,27 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/", methods=['GET', 'POST'])
 def query():
+    query_list = ['id', 'content', 'station_id', 'url', 'name', 'local_name',       \
+            'status', 'type', 'processing', 'service', 'callsign', 'partycity',     \
+            'invoice_in_name', 'invoice_in_content', 'uploaded_timestamp',          \
+            'downloaded_timestamp', 'meta_creation', 'meta_modification', 's3_name']
     if request.method == 'POST':
         try:
-            query = request.form['query']
-            result = es.search(query=query, index='documents')
+            q = {"query":{"match_all":{}}}
+            req = string.replace(request.form['query'], "'", '"')
+            print request.form
+            a = request.form['select']
+            print request.form.getlist('select'), 'list'
+            
+            for item in request.form.getlist('select'):
+                print request.form[item]
+            
+            query = json.loads(json.loads(json.dumps(req)))
+            result = json.dumps(es.search(query=query, index='documents'))
         except:
-            return render_template('query.html', error=query)
-        return render_template('query.html', result=result)
-    return render_template('query.html')
+            return render_template('query.html', error=q, lists=query_list)
+        return render_template('query.html', result=result, lists=query_list)
+    return render_template('query.html', lists=query_list)
 
 @app.route("/query", methods=['GET', 'POST'])
 def home():
